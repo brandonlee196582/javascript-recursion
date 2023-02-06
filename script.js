07c047e0e784865461c6671aaba4a0327359db1c
brandonLee <brandonlee196582@gmail.com>
2023-02-06 10:39:14 -0700

    started a new recursive function

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 
-// Write a recursive function that takes an integer count and returns an array containing integers from 0 to count (inclusive).
+// Write a recursive function that takes an integer count and 
+//returns an array containing integers from 0 to count (inclusive).
 //     ex. - when countUp(3) is called, the output should be: [0, 1, 2, 3]
 
-var countUp = (count) => {
-    // If count is not an array convert it to an array, assign the initial value of count as the first array value of the array and call countUp.
+var countUp = count => {
+    // If count is not an array convert it to an array, assign 
+    // the initial value of count as the first array value of
+    // the array and call countUp.
     if (Array.isArray(count) === false) {
         let countStart = count;
         count = [];
@@ -11,7 +14,9 @@ var countUp = (count) => {
         countUp(count);
         return count;
     } else {
-        // If count is an array and first value is greater than 0, insert a new array item at the start of the array that is one less than the previous number.
+        // If count is an array and first value is greater than
+        // 0, insert a new array item at the start of the array
+        // that is one less than the previous number.
         if (count[0] > 0) {
             count.unshift(count[0] - 1);
             countUp(count);
@@ -22,3 +27,13 @@ var countUp = (count) => {
 console.log(countUp(5));
 
 
+//Write a function that generates every possible sequence of
+//throws a single player could throw over a game of
+//rock-paper-scissors based on the number of rounds passed
+//into the function.
+
+var rockPaperScissors = rounds => {
+
+};
+
+console.log(rockPaperScissors(3));
